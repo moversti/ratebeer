31afad9b62946ef1b83379e3792ba279046a40a2
Mikko Översti <moversti92@gmail.com>
2014-02-16 14:58:55 +0200

    skadughiudsghidgshihdgsi

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -3,6 +3,9 @@ class Brewery < ActiveRecord::Base
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 
+  validates :name, presence: true
+  validates :year, numericality: {greater_than_or_equal_to: 1042,
+                                  only_integer: true}
 
 
 end
